02ac500478ad1e3a38d4d65c2729a13db5c5c0d9
smira2022 <sheenapmiranda@yahoo.com>
2024-12-05 18:04:57 -0700

    Animated container demo first commit

---
+++ android/app/src/main/kotlin/com/example/animatedcontainer1205/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animatedcontainer1205
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+double randomBorderRadius() {
+  return Random().nextDouble() * 64;
+}
+
+double randomMargin() {
+  return Random().nextDouble() * 64;
+}
+
+Color randomColor() {
+  return Color(0xFF000000 |
+      Random().nextInt(0xFFFFFF)); // Changed to include alpha channel
+}
+
+class AnimatedContainerExample extends StatefulWidget {
+  const AnimatedContainerExample({super.key});
+
+  @override
+  State<AnimatedContainerExample> createState() =>
+      _AnimatedContainerExampleState();
+}
+
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
+  late Color color;
+  late double borderRadius;
+  late double margin;
+
+  @override
+  void initState() {
+    super.initState();
+    color = randomColor();
+    borderRadius = randomBorderRadius();
+    margin = randomMargin();
+  }
+
+  void change() {
+    setState(() {
+      color = randomColor();
+      borderRadius = randomBorderRadius();
+      margin = randomMargin();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedContainer(
+              width: 128,
+              height: 128,
+              margin: EdgeInsets.all(margin),
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+              duration: Duration(milliseconds: 400),
+            ),
+            ElevatedButton(
+              child: const Text('Change'),
+              onPressed: () => change(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedContainerExample(),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
